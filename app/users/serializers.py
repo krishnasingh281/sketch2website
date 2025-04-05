2cd37da03ac934bf068ceea2f8d9f6400a50d618
krishnasingh281 <samar.phone.home@gmail.com>
2025-04-06 03:13:46 +0530

    updated serializers

--- app/users/serializers.py
+++ app/users/serializers.py
@@ -10,20 +10,30 @@ class UserSeriailzer(serializers.ModelSerializer):
         fields = ['username','email', 'password', 'profile_picture','bio', 'first_name', 'last_name']
         read_only_fields = ('id',) 
         
-class RegisterSeriailzer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only = True, required = True, validators = [validate_password])
-    password2 = serializers.CharField(write_only = True, required = True)
-    
-    class Meta: 
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
         model = User
         fields = ('username', 'email', 'password', 'password2', 'profile_picture')
         extra_kwargs = {'email': {'required': True}}
-        
-        def validate(self, data):
-            if data['password'] != data['password2']:
-                raise serializers.ValidationError({'password': 'Passwords do not match'})
-            
-            return data
 
-    
-    
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError({'password': 'Passwords do not match'})
+        return data
+
+    def create(self, validated_data):
+        # Remove password2 from the validated data
+        validated_data.pop('password2')
+
+        # Create user instance without saving password yet
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            profile_picture=validated_data.get('profile_picture')
+        )
+        user.set_password(validated_data['password'])  # Proper password hashing
+        user.save()
+        return user
