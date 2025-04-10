2318b3f9e66aa928b5882678bd51e8f813980688
krishnasingh281 <samar.phone.home@gmail.com>
2025-04-11 00:22:48 +0530

    added proxy

--- proxy/run.sh
+++ proxy/run.sh
@@ -2,5 +2,8 @@
 
 set -e
 
+# Replace environment variables in the template
 envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
+
+# Start nginx in foreground
 nginx -g 'daemon off;'
