145e02ae7a7304655cdc02aa280fdbf9781cc26a
krishnasingh281 <samar.phone.home@gmail.com>
2025-04-12 02:51:46 +0530

    final commit

--- app/app/settings.py
+++ app/app/settings.py
@@ -22,7 +22,7 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 
 
-SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 0)))
