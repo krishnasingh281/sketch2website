71050417585ca90f260278ada498b6a509a133bd
krishnasingh281 <samar.phone.home@gmail.com>
2025-04-12 03:11:20 +0530

    final commit

--- proxy/run.sh
+++ proxy/run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -e
 
-envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
-nginx -g 'daemon off;'
+# Replace only required variables to avoid empty ones staying as-is
+envsubst '${LISTEN_PORT} ${APP_HOST} ${APP_PORT}' < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
+
+exec nginx -g "daemon off;"
